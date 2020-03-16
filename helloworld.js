5ee513f6d5ceeb2e42745b8c08532fbf0e92bd6c
Danilo Rêgo <daniloapr@gmail.com>
2020-03-16 20:31:11 -0300

    feat: helloworld.js

---
+++ helloworld.js
@@ -0,0 +1,8 @@
+const http = require("http");
+const server = http.createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write("Hello, World!");
+    response.end(); //boa prática no node
+});
+
+server.listen(8081); //acessar localhost:8081
